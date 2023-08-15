d01721e759dd1e15261080a441961c29505e4adb
Sviatoslav <116236590+SamNeobast@users.noreply.github.com>
2023-08-16 00:00:41 +0300

    Add ShakeCamera after shoot

--- Assets/Scripts/Game/Gun/ShootingPistol.cs
+++ Assets/Scripts/Game/Gun/ShootingPistol.cs
@@ -5,7 +5,10 @@ public class ShootingPistol : MonoBehaviour
     [SerializeField] private GameObject bullet;
     [SerializeField] private float timeRechange;
     [SerializeField] private Transform firePoint;
-  
+    [SerializeField] private float shakeIntensivity = 5f;
+    [SerializeField] private float timeShake = 2f;
+
+
     private bool canShoot;
     private float timer;
 
@@ -25,6 +28,7 @@ public class ShootingPistol : MonoBehaviour
         if (Input.GetMouseButton(0) && canShoot)
         {
             Instantiate(bullet, firePoint.position, firePoint.rotation);
+            ShakeCinemashine.Instance.ShakeCamera(shakeIntensivity, timeShake);
             canShoot = false;
         }
     }
